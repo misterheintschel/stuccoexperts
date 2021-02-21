f7da30702d83a08173963c9473bf8cfa20661524
John Heintschel <john@checkhq.com>
2021-02-20 22:01:33 -0500

    add react router

--- stuccoexperts/src/Body.jsx
+++ stuccoexperts/src/Body.jsx
@@ -1,3 +1,10 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 const description = 'With more than 40 years of ' +
   'experience, we provide Southern California with ' +
   'high quality stucco, expert craftsmanship, and ' +
@@ -10,20 +17,29 @@ const paragraph = "The Stucco Experts' Team is currently " +
   "the contact information on our Contact Us Page.";
 
 const Body = () => (
-    <div class="main">
-    <div class="placeholder">
-        <h2>{description}</h2>
-        <div class="background_img">
-            <img src="logo.png" alt="" />
+  <Router>
+    <Switch>
+      <Route path="/marcus">
+       <h1>MARCUSMARCUSMARCUS</h1>
+      </Route>
+      <Route path="/asdf">
+        <div class="main">
+          <div class="placeholder">
+            <h2>{description}</h2>
+              <div class="background_img">
+                  <img src="logo.png" alt="" />
+              </div>
+          </div>
+          <div class="message">
+              <p>{paragraph}</p>
+          </div>
+          <div class="contactus">
+              <a href="contact.html"><li>Contact Us</li></a>
+          </div>
         </div>
-    </div>
-    <div class="message">
-        <p>{paragraph}</p>
-    </div>
-    <div class="contactus">
-        <a href="contact.html"><li>Contact Us</li></a>
-    </div>
-    </div>
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default Body;
--- stuccoexperts/src/Header.jsx
+++ stuccoexperts/src/Header.jsx
@@ -1,20 +1,18 @@
 import './Header.css';
 const Header = () => (
-        <div className="navbar">
-            <div className="container flex">
-
-                <h1 className="logo">Stucco Experts Inc.</h1>
-                <nav>
-                    <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="aboutus.html">About Us</a></li>
-                        <li><a href="getaquote.html">Get a Quote</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
-                    </ul>
-                </nav>
-            </div>
-
-        </div>
+  <div className="navbar">
+    <div className="container flex">
+      <h1 className="logo">Stucco Experts Inc.</h1>
+      <nav>
+      <ul>
+      <li><a href="marcus">Home</a></li>
+      <li><a href="aboutus.html">About Us</a></li>
+      <li><a href="getaquote.html">Get a Quote</a></li>
+      <li><a href="contact.html">Contact Us</a></li>
+      </ul>
+      </nav>
+    </div>
+  </div>
 );
 
 export default Header;
